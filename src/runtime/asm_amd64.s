a85b61f188f72d6a76a2f46ac1534326caba622f
doujiang24 <doujiang24@gmail.com>
2022-03-21 17:20:17 +0800

    set cgo_drop function.

--- src/runtime/asm_amd64.s
+++ src/runtime/asm_amd64.s
@@ -862,6 +862,25 @@ nosave:
 GLOBL zeroTLS<>(SB),RODATA,$const_tlsSize
 #endif
 
+// func cgodropm()
+TEXT cgodropm(SB),NOSPLIT,$0-0
+	PUSH_REGS_HOST_TO_ABI0()
+
+	get_tls(CX)
+#ifdef GOOS_windows
+	MOVL	$0, BX
+	CMPQ	CX, $0
+	JEQ	2(PC)
+#endif
+	MOVQ	g(CX), BX
+	CMPQ	BX, $0
+	JNE	done
+	MOVQ	$runtime·dropm(SB), AX
+	CALL	AX
+done:
+	POP_REGS_HOST_TO_ABI0()
+	RET
+
 // func cgocallback(fn, frame unsafe.Pointer, ctxt uintptr)
 // See cgocall.go for more details.
 TEXT ·cgocallback(SB),NOSPLIT,$24-24
@@ -1003,7 +1022,11 @@ havem:
 	JNE	done
 	MOVQ	_cgo_pthread_key_created(SB), AX
 	CMPQ	(AX), $0
-	JNE	done
+	JEQ	dropm
+	MOVQ	_cgo_dropm(SB), AX
+	MOVQ	$cgodropm(SB), BX
+	MOVQ	BX, (AX)
+dropm:
 	MOVQ	$runtime·dropm(SB), AX
 	CALL	AX
 #ifdef GOOS_windows
