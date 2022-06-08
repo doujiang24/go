f64f3245808de1dbfa1e694a2d4d44b8a8e9a132
doujiang24 <doujiang24@gmail.com>
2022-06-08 16:37:37 +0800

    remove name.

--- src/cmd/trace/trace.go
+++ src/cmd/trace/trace.go
@@ -1136,6 +1136,7 @@ func (ctx *traceContext) buildBranch(parent frameNode, stk []*trace.Frame) int {
 func isSystemGoroutine(entryFn string) bool {
 	// This mimics runtime.isSystemGoroutine as closely as
 	// possible.
+	// Also, locked g in extra M is system goroutine.
 	return entryFn == "" || entryFn != "runtime.main" && strings.HasPrefix(entryFn, "runtime.")
 }
 
